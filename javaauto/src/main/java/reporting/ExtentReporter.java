bb58c464dac6c9bb48593c313359a400831a333d
rj-github-ac <rj97469@gmail.com>
2022-12-10 20:10:12 +0530

    Extent Reports class singleton desing

--- javaauto/src/main/java/reporting/ExtentReporter.java
+++ javaauto/src/main/java/reporting/ExtentReporter.java
@@ -5,13 +5,14 @@ import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 
 public class ExtentReporter {
 	
-	public static ExtentReports extent= null;
+	private static ExtentReports extent= null;//private static instance to comply the singleton design 
 	
 	private ExtentReporter()
 	{
-		
+		//private constructor to comply the singleton design
 	}
 	
+	//public static method to return the class instance
 	public static ExtentReports getInstance()
 	{
 		if(extent==null)
